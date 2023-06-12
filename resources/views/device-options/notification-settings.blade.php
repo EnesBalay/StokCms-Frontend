9d0bab5f000c5f73eb18ac03780debdec532c629
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-06-12 16:22:39 +0300

    notificaiton settings added

--- resources/views/device-options/notification-settings.blade.php
+++ resources/views/device-options/notification-settings.blade.php
@@ -1,4 +1,35 @@
 @extends('layouts.app')
 @section('content')
-<h1>Bildirim Ayarları</h1>
+<div class="content-wrapper">
+    <div class="d-flex justify-content-between mb-2">
+        <h3 class="text-primary">Bildirim Ayarları</h3>
+        <button onclick="modal('add')" class="btn btn-sm btn-primary btn-icon-text">Yeni Ekle</button>
+    </div>
+    <div class="card">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">E-Posta</th>
+                    <th scope="col">Telefon</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>test@test.com</td>
+                    <td>123123123</td>
+                    <td>
+                        <button onclick="modal('edit')" class="btn btn-sm btn-light btn-icon-text">
+                            <i class="mdi mdi-pencil"></i>
+                        </button>
+                        <button class="btn btn-sm btn-primary btn-icon-text">
+                            <i class="mdi mdi-trash-can"></i>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <script src="/js/notification-settings/modal.js"></script>
+</div>
 @endsection
