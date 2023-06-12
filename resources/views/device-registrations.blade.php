d8fb2fa501ea5710015b8776e8fe9042e9e0a629
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-06-12 15:13:47 +0300

    cihaz tanimlama sayfasi

--- resources/views/device-registrations.blade.php
+++ resources/views/device-registrations.blade.php
@@ -1,4 +1,32 @@
 @extends('layouts.app')
 @section('content')
-<h1>Cihaz Tanımlama</h1>
+<div class="content-wrapper">
+    <div class="d-flex justify-content-between mb-2">
+        <h3 class="text-primary">Cihaz Tanımlama</h3>
+    </div>
+    <div class="card">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Marka</th>
+                    <th scope="col">Model</th>
+                    <th scope="col">Versiyon</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Foo tech</th>
+                    <th>Fr3</th>
+                    <th>1.23</th>
+                    <td>
+                        <a href="/cihaz-tanimlama/1/edit" class="btn btn-sm btn-primary btn-icon-text">
+                            <i class="mdi mdi-open-in-new"></i>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
