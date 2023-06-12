145ebb827cd92145ff13d3bd2ca6f522d0df5e1b
Enes Balay <62758732+EnesBalay@users.noreply.github.com>
2023-06-12 13:56:20 +0300

    sidebar routes updated

--- resources/views/components/_sidebar.blade.php
+++ resources/views/components/_sidebar.blade.php
@@ -25,9 +25,80 @@
       </a>
     </li>
     <li class="nav-item">
+      <a class="nav-link" href="/kritik-stok">
+        <i class="mdi mdi-call-missed menu-icon"></i>
+        <span class="menu-title">Kritik Stok</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/cihaz-tanimlama">
+        <i class="mdi mdi-bookmark menu-icon"></i>
+        <span class="menu-title">Cihaz Tanımlama</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/malzeme-degisimleri">
+        <i class="mdi mdi-bookmark menu-icon"></i>
+        <span class="menu-title">Malzeme Değişimleri</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/uretim">
+        <i class="mdi mdi-bookmark menu-icon"></i>
+        <span class="menu-title">Üretim</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="collapse" href="#devices" aria-expanded="false" aria-controls="tables">
+        <i class="icon-grid-2 menu-icon"></i>
+        <span class="menu-title">Cihaz Seçenekleri</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="devices">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item">
+            <a class="nav-link" href="/cihaz-markalari">Cihaz Markaları</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/cihaz-modelleri">Cihaz Modelleri</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/cihaz-versiyonlari">Cihaz Versiyonları</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/raf-gruplari">Raf Grupları</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/raf-numaralari">Raf Numaraları</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/malzeme-durumlari">Malzeme Durumlari</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/temin-edilen-firmalar">Firmalar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/bildirim-ayarlari">Bildirim Ayarları</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item">
       <a class="nav-link" href="/raporlar">
         <i class="mdi mdi-book-multiple menu-icon"></i>
         <span class="menu-title">Raporlar</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/kullanicilar">
+        <i class="mdi mdi-account-multiple menu-icon"></i>
+        <span class="menu-title">Kullanıcılar</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/ayarlar">
+        <i class="mdi mdi-settings menu-icon"></i>
+        <span class="menu-title">Ayarlar</span>
       </a>
     </li>
   </ul>
