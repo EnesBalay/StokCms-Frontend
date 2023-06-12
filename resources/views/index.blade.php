fba85e7bb121b7d9c4642327270fa7ec5beba64f
Enes Balay <62758732+EnesBalay@users.noreply.github.com>
2023-06-12 13:38:38 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
     <div class="row">
         <div class="col-md-12 grid-margin transparent">
             <div class="row">
-                <div class="col-md-6 mb-4 stretch-card transparent">
+                <div class="col-md-4 mb-4 stretch-card transparent">
                     <div class="card card-tale">
                         <div class="card-body">
                             <p class="mb-4">Toplam Üretim</p>
@@ -23,7 +23,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 mb-4 stretch-card transparent">
+                <div class="col-md-4 mb-4 stretch-card transparent">
                     <div class="card card-dark-blue">
                         <div class="card-body">
                             <p class="mb-4">Toplam Maliyet</p>
@@ -31,24 +31,15 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
-                    <div class="card card-light-blue">
+                <div class="col-md-4 mb-4 stretch-card transparent">
+                    <div class="card card-light-danger">
                         <div class="card-body">
                             <p class="mb-4">Kritik Stok</p>
                             <p class="fs-30 mb-2">0 Ürün</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 stretch-card transparent">
-                    <div class="card card-light-danger">
-                        <div class="card-body">
-                            <p class="mb-4">Number of Clients</p>
-                            <p class="fs-30 mb-2">47033</p>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
 
